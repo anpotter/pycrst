16d9afcab0d08f0c1ba3d76cc30a4a56dd67c1cb
Andrew Potter <apotter1@una.edu>
2023-06-23 16:59:28 -0500

    Add files via upload

--- pcpp.py
+++ pcpp.py
@@ -1,15 +1,5 @@
 # Pretty crummy pretty print for relational propositions
 
-"Not lazy"
-#exp ='background(volitional-result(1,circumstance(3,2)),evidence(concession(5,antithesis(7,6)),4))'
-#exp = 'background(volitional-result(1,circumstance(3,2)),evidence(concession(5,antithesis(7,6)),4))'
-
-#CC Letter
-#exp = 'motivation(conjunction(evidence(justify(10,concession(11,antithesis(12,13))),1),evidence(antithesis(evidence(contrast(5,6),concession(2,3)),elaboration(9,condition(8,7))),1)),14)'
-
-
-
-
 def pcpp(exp):
     indent = 0;
     ppexp = ""
@@ -38,9 +28,11 @@ def pcpp(exp):
         comma = False        
         if c == ',':
             comma = True
-            
-            
+
     return(ppexp)
 
-#print(pcpp(exp))
+if __name__ == "__main__":
+    "Not lazy"
+    exp = 'background(volitional-result(1,circumstance(3,2)),evidence(concession(5,antithesis(7,6)),4))'
+    print(pcpp(exp))
 
